01770d63ba37ac6f9d81fad8f4e1aad42893e89c
Brian Guthrie <brianguthrie88@aol.com>
2024-03-07 21:04:13 -0500

    added API routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const userExerciseRoutes = require('./userExerciseRoutes');
+const exerciseRoutes = require('./exerciseRoutes');
+
+router.use('/users', userRoutes);
+router.use('/userExercise', userExerciseRoutes);
+router.use('/exercises', exerciseRoutes);
+
+module.exports = router;
